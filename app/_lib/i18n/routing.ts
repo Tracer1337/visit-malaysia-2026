663162e094bfd8bed5a99a74043a94475f2b608f
Tracer1337 <merlin.moelter@gmail.com>
2024-12-04 19:16:19 +0100

    Fix Next To legacy-react-app Rewrites

--- app/_lib/i18n/routing.ts
+++ app/_lib/i18n/routing.ts
@@ -1,7 +1,5 @@
 import { createNavigation } from 'next-intl/navigation';
 import { defineRouting } from 'next-intl/routing';
-// eslint-disable-next-line no-restricted-imports
-import NextLink from 'next/link';
 import { notFound } from 'next/navigation';
 import { ComponentProps, createElement } from 'react';
 import { Locale, appConfig } from '@/../config';
@@ -15,9 +13,12 @@ const i18nNavigation = createNavigation(routing);
 
 export function Link(props: ComponentProps<(typeof i18nNavigation)['Link']>) {
   return createElement(
-    props.href.toString().startsWith('/legacy')
-      ? (NextLink as (typeof i18nNavigation)['Link'])
-      : i18nNavigation.Link,
+    /**
+     * Rewrites do not work with client-side navigation on vercel.
+     * The following "a"-Tag may be replaced by the next Link component
+     * when deployed to a different environment.
+     */
+    props.href.toString().startsWith('/legacy') ? 'a' : i18nNavigation.Link,
     props,
   );
 }
